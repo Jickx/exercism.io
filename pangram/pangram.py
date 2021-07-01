ba3d46fe39040b64c8bbee1b759e68349783b9f4
Jickx <63093696+Jickx@users.noreply.github.com>
2021-07-01 22:30:25 +0300

    pangram done

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,5 +1,15 @@
+import string
+
 def is_pangram(sentence):
-    
-    
-    
-    pass
+    alphabet = list(string.ascii_lowercase)
+    for i in sentence.lower():
+        if i in alphabet:
+            alphabet.remove(i)
+    if not alphabet:
+        return True
+    else:
+        return False
+
+
+
+
